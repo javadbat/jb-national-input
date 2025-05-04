64241ef76b0c29f54f187fdcce8a2cd9604c8ecb
javadbat <javadbat@gmail.com>
2025-05-04 11:51:53 +0330

    update demo path

--- stories/JBNationalInput.stories.tsx
+++ stories/JBNationalInput.stories.tsx
@@ -2,7 +2,7 @@ import { JBNationalInput, Props } from 'jb-national-input/react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<Props> = {
-  title: "Example/form elements/Inputs/JBNationalInput",
+  title: "Components/form elements/Inputs/JBNationalInput",
   component: JBNationalInput,
 };
 export default meta;
